7c64ec310f0b68c0ae5998888cf44f52f40162a8
Shane O. <shaneaorth@gmail.com>
2026-09-23 00:38:55 -0400

    Update demote group for Arms Dealers
    Removed TEAM_RUSTUNG from the Arms Dealers demote group.

--- lua/darkrp_customthings/demotegroups.lua
+++ lua/darkrp_customthings/demotegroups.lua
@@ -28,4 +28,5 @@ DarkRP.createDemoteGroup("Resistance", {
 
 DarkRP.createDemoteGroup("Arms Dealers", {
     TEAM_BLACKMARKET, TEAM_CHERKESOV, TEAM_SUPPLIER,
-})[[TEAM_RUSTUNG, ]]
+})
+--[[TEAM_RUSTUNG, ]]
